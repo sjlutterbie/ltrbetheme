d165d2ee1bf3814e7a9cbbc759d330117cc875f4
Simon Lutterbie <simon@ltrbe.com>
2013-04-24 08:03:09 -0700

    Removed height requirement & post date

--- content-single.php
+++ content-single.php
@@ -7,11 +7,11 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
+	<h1 class="entry-title"><?php the_title(); ?></h1>
 
 		<div class="entry-meta">
-			<?php ltrbetheme_posted_on(); ?>
-		</div><!-- .entry-meta -->
+	<!--		<?php ltrbetheme_posted_on(); ?> -->
+		</div><!-- .entry-meta --> 
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
